cb795017121ff1d2e7548651bca341cbdff47b0f
nowcastsystem <53198440+nowcastsystem@users.noreply.github.com>
2019-09-13 11:29:07 -0500

    Add files via upload

---
+++ app4.py
@@ -0,0 +1,79 @@
+
+import plotly.graph_objs as go
+import pandas as pd
+
+import dash_core_components as dcc
+import dash
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+data = pd.read_csv('./prediction.csv',
+                   index_col=0)
+
+
+prediction = go.Scatter(
+    x=data.index,
+    y=data['prediction'],
+    name="Predicted Value",
+    line=dict(color='#17BECF'),
+    opacity=0.8)
+
+actualtraffic = go.Scatter(
+    x=data.index,
+    y=data['outcome'],
+    name="Actual Value",
+    line=dict(color='#7F7F7F'),
+    opacity=0.8)
+
+fig = go.Figure(
+    data=[prediction, actualtraffic],
+    layout=go.Layout(
+        title='',
+    )
+)
+
+fig.update_layout(
+    xaxis=go.layout.XAxis(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=6,
+                     label="6m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="YTD",
+                     step="year",
+                     stepmode="todate"),
+                dict(count=1,
+                     label="1y",
+                     step="year",
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        rangeslider=dict(
+            visible=True
+        ),
+        type="date"
+    )
+)
+
+app.layout = html.Div(children=[
+    html.H1(children='Forecasting Result'),
+    dcc.Graph(
+        figure=fig
+    )
+])
+
+
+if __name__ == '__main__':
+    app.run_server(host='127.0.0.1', port=808, debug=True)
+
+
